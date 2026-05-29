53523f51fff412844295352eba4b10c40dd912e5
mavmateo <mavmateo7@gmail.com>
2026-05-29 11:56:52 +0000

    pyspark functional

--- src/process/spark_stream.py
+++ src/process/spark_stream.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import logging
 from config.kafka_config import KafkaConfig
@@ -9,32 +10,40 @@ from config.kafka_config import KafkaConfig
 logger = logging.getLogger(__name__)
 from pyspark.sql import SparkSession
 
+
 def _create_spark_session(config: KafkaConfig) -> SparkSession:
     builder = SparkSession.builder.appName("MarketStreamApp")
-    builder = builder.config("spark.jars.packages, org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")
-
-
-               
+    builder = builder.config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1")
+    builder = builder.config("spark.sql.streaming.checkpointLocation", "/tmp/checkpoint/marketstream")
+    #builder = builder.config("spark.sql.adaptive.enabled", "true")           
     spark = builder.getOrCreate()
 
-
-    print("="*60)
-    print(f"Spark version: {spark.version}")
-    print("="*60)
-
+    spark.sparkContext.setLogLevel("WARN")
+    logger.info("="*75)
+    logger.info("Spark version: %s",spark.version)
+    logger.info("Spark Session created successfully!")
+    logger.info("="*75)
     return spark
 
 
-def _read_stream(spark: SparkSession, config: KafkaConfig) -> None:
-    rawstocks_stream = (spark.readStream
-                        .format("kafka")
-                        .option("kafka_bootstrap_servers",config.bootstrap_servers)
-                        .option("subscribe", config.RAW_STOCKS_TOPIC)
-                        .option("startingOffsets", "latest")
-                        .option("failOnDataLoss", "false")
-                        .load()
-                        )
-    return rawstocks_stream
+def _read_stream(spark: SparkSession, config: KafkaConfig):
+    try:
+        rawstocks_stream = (spark.readStream
+                            .format("kafka")
+                            .option("kafka.bootstrap.servers",",".join(config.bootstrap_servers))
+                            .option("subscribe",f"{config.RAW_STOCKS_TOPIC},{config.RAW_CRYPTO_TOPIC}")
+                            .option("startingOffsets", "latest")
+                            .option("failOnDataLoss", "false")
+                            .load()
+                            )
+        logger.info("Kafka stream created for topic: %s and %s", config.RAW_STOCKS_TOPIC, config.RAW_CRYPTO_TOPIC)
+        return rawstocks_stream
+    
+    except Exception as e:
+        logger.error(f"Failed to read from kafka, {e}", exc_info=True)
+        raise
+    
+    
     
 
     
@@ -48,10 +57,31 @@ def _read_stream(spark: SparkSession, config: KafkaConfig) -> None:
 #def _write_s3() :
 
 def main() -> None: 
-   spark =  _create_spark_session(KafkaConfig)
-   raw_stream = _read_stream(spark, KafkaConfig) 
-   print("Streaming schema:")
-   raw_stream.printSchema()
+   try:
+        kafka_config = KafkaConfig()
+        spark = _create_spark_session(kafka_config)
+
+        logger.info("="*75)
+        logger.info("Stream reading process initiated...")
+        logger.info("="*75)
+
+        rawstocks_stream = _read_stream(spark, kafka_config) 
+
+
+        query = (rawstocks_stream
+                    .writeStream
+                    .format("console")
+                    .option("truncate", False)
+                    .start())
+        
+        query.awaitTermination()
+   except KeyboardInterrupt:
+        logger.info("Streaming stopped by user")
+   except Exception as e:
+        logger.critical("Application failed", exc_info=True)  
+        sys.exit(1)  
+        
+   #raw_stream.printSchema()
 
 
 
